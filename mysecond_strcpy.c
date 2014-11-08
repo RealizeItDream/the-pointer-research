83f0d01dc0052eab773c139b40fd8d5c401c7edb
Liu Xiangyu <RealizeItDream@users.noreply.github.com>
2014-11-08 23:04:23 +0800

    复制字符串（指针实现）可以判空

--- mysecond_strcpy.c
+++ mysecond_strcpy.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+#include<assert.h>
 
 char * my_strcpy(char *dest, const char * src)
 {
 	char *ret = dest;
+	assert((dest != NULL) && (src != NULL));      //用来进行判空
 	while((*dest++ = *src++));
 	return dest;
 }
